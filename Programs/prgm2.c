6509ba306639347fb2330fc31774d9d552606d5e
Deva Nanda C <nc07deva@gmail.com>
2026-01-19 22:57:55 +0530

    Create prgm2.c

---
+++ Programs/prgm2.c
@@ -0,0 +1,26 @@
+//To express the pH of substances
+
+#include <stdio.h>
+int main()
+{
+    int pH;
+    printf("Enter the pH : ");
+    scanf("%d", &pH);
+    if(pH>7)
+    {
+        if(pH<12)
+            printf("Alkaline");
+        else
+            printf("Very alkaline");
+    }
+    else if (pH==7)
+        printf("Neutral");
+    else
+    {
+        if(pH>2)
+            printf("Acidic");
+        else
+            printf("Very acidic");
+    }
+    return 0;
+}
